8b8cfa763b9964132937b1b15e4508a0ea85c559
kanyarcy <kanwaanir11@gmail.com>
2025-01-10 18:40:23 +0530

    fix port

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const app = express();
 
+const PORT = 9000;
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(PORT, () => console.log("Server ready on port ", PORT));
 
 export default app;
